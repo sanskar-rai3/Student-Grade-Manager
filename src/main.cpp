a143d11783578491d109a26dc37a6f7d4d07a00a
Sanskar Rai <sanskarr6812@gmail.com>
2026-02-17 15:35:24 +0545

    start

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+#include "student.h"
+
+#include <iostream>
+#include <vector>
+
+int main(int argc, char *argv[]) {
+    std::cout << "Hello world" << std::endl;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
--- src/student.cpp
+++ src/student.cpp
@@ -1,3 +1,11 @@
 #include <student.h>
 
-Student::Student(std::)
+Student::Student(const std::string &name, float gpa) : name(name), gpa(gpa) {};
+
+std::string Student::getName() const {
+    return name;
+}
+
+float Student::getGPA() const {
+    return gpa;
+}
